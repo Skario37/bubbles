ca50b81357ac18ec82ac331b685f8717fa426304
Kevin <kevin.courriels@gmail.com>
2021-09-20 21:51:00 +0200

    WIP

--- engine.js
+++ engine.js
@@ -1,48 +1,33 @@
-/**
- * Viscosity
- * https://en.wikipedia.org/wiki/Viscosity#Dynamic_viscosity
- * 
- * Density
- * https://en.wikipedia.org/wiki/Density_of_air
- * https://en.wikipedia.org/wiki/Properties_of_water#Density_of_water_and_ice
- * 
- * Pressure
- * 
- * Other
- * https://www.chimix.com/an10/sup10/itpe11.htm#:~:text=Ecrire%20l'%C3%A9quation%20dynamique%20de,f%2C%20verticale%20vers%20le%20bas.
- */
-
+const AIR_MOLAR_MASS = 0.02896; // kg/mol
+const WATER_MOLAR_MASS = 0.01801; // kg/mol
+const GRAVITY_EARTH = 9.80665; // m.s-2
+const ATMOSPHERIC_PRESSURE = 101325; // Pa
+const IDEAL_GAS = 8.3144; // J.K-1.mol-1
+const KELVIN = 273.15; 
 
 const AIR = 0;
-const WATER = 0;
+const WATER = 1;
 const AREAS =  {
   "air": {
     "type": AIR,
     /**
      * @param {Float} t in °C
      * @param {Float} p in Pa
-     * @param {Float} h between 0 and 1
      * @returns
      */
-    "mass": (t, p, h) => {
+    "density": (t, p) => {
       t = t || 27;
-      p = p || 101325;
-      h = h || 0.5;
-      return (1/(287.06*(t+273.15)))*(p-230.617*h*Math.exp((17.5043*t)/(241.2+t)));
+      p = p || ATMOSPHERIC_PRESSURE;
+      return (p*AIR_MOLAR_MASS) / (IDEAL_GAS*(t + KELVIN))
     },
     /**
      * @param {Number} a in m
      */
     "pressure": (a) => {
       a = a || 0;
-      return 101325*Math.pow(1-2.25577*Math.pow(10,-5)*a,5.255);
+      return ATMOSPHERIC_PRESSURE*Math.pow(1-2.25577*Math.pow(10,-5)*a,5.255);
     },
-    "humidity": 0.5,
-    "temperature": 27,
-    "viscosity": (t) => {
-      t = t || 27;
-      return 0.0000002791*Math.pow(t+273.15,0.7355);
-    }
+    "temperature": 27
   },
   "water": {
     "type": WATER,
@@ -51,27 +36,15 @@ const AREAS =  {
      * @param {Float} s in kg/m3
      * @returns 
      */
-    "mass": (t, s) => {
+    "density": (t, s) => {
       t = t || 18;
       s = s || 10;
       return 1000-0.12*t+0.35*s;
     },
-    /**
-     * @param {Number} a in m
-     */
-    "pressure": (a) => {
-      a = a || 0;
-      return 101325+a/10*101325;
-    },
     "temperature": 18,
     "salinity": 10,
-    "viscosity": (t) => {
-      t = t || 18;
-      return 0.02939*Math.exp(507.88/(t-149.3));
-    }
   }
 }
-const GRAVITY_EARTH = 9.80665; // N/kg
 
 class Engine {
   _container = undefined;
@@ -94,7 +67,7 @@ class Engine {
     this._container = c;
     this._container.appendChild(this._canvas);
 
-    this._canvas.style.position = "absolute";
+    this._canvas.style.position = "relative";
     this._canvas.style.top = 0;
     this._canvas.style.left = 0;
 
@@ -120,6 +93,7 @@ class Engine {
   createBubble = (n, s) => {
     for (let i = 0; i < n; i++) {
       this._bubbles.push(new Bubble(this._ctx, this._width, this._height, s));
+      if (this._area.type === AIR) this._bubbles[i].setColor("#ff0000");
     }
   }
 
@@ -141,26 +115,24 @@ class Engine {
     }
     this._bubbles.sort((bubble1, bubble2) => bubble1.scaleProjected - bubble2.scaleProjected);
 
-    if (this._area.type === AIR) {
-      for (let i = 0; i < this._bubbles.length; i++) {
-        const pos = this._bubbles[i].getPosition();
-        const bubble_volume = this._bubbles[i].getSurface();
-        const env_density = this._area.mass(
-          this._area.temperature, 
-          this._area.pressure(this._height - pos.y),
-          this._area.humidity
-        );
-        const bubble_weight = bubble_volume * env_density * GRAVITY_EARTH;
-        const f = 6 * Math.PI * this._area.viscosity(this._area.temperature) * (this._bubbles[i].getRadius()) * bubble_weight; // force de frottement
-        pos.y -= f / 1000; // 5.11e-9kg*m/s² = 5.11e-6g*m/s
-        this._bubbles[i].updatePosition(pos.x, pos.y, pos.z);
-        this._bubbles[i].draw();
-        this._detectCollision(i);
-      }
-    } else if (this._area.type === WATER) {
-      // todo
+    for (let i = 0; i < this._bubbles.length; i++) {
+      const pos = this._bubbles[i].getPosition();
+      const bubble_volume = this._bubbles[i].getSurface();
+      
+      const bubble_density = AREAS.air.density() * 0.999 + AREAS.water.density() * 0.001;
+      const g_force = bubble_density * bubble_volume * GRAVITY_EARTH;
+
+      const area_density = this._area.density();
+      const p_force = area_density * bubble_volume * GRAVITY_EARTH;
+
+      pos.y += convertMeterToPixel(g_force - p_force) / 1000;
+
+      // frottement = -6pi*r*v
+
+      this._bubbles[i].updatePosition(pos.x, pos.y, pos.z);
+      this._bubbles[i].draw();
+      this._detectCollision(i);
     }
-    
   }
 
   _detectCollision = (i) => {
@@ -192,9 +164,10 @@ class Bubble {
   getSurface = () => this._surface;
   getPosition = () => { return { "x": this._x, "y": this._y, "z": this._z } };
   getRadius = () => this._radius;
+  setColor = (c) => this._color = c;
 
   create = () => {
-    this._perspective = this._width * 0.9;
+    this._perspective = this._width * 1;
     this._projection_center_x = this._width / 2;
     this._projection_center_y = this._height / 2;
 
@@ -208,11 +181,11 @@ class Bubble {
     this.scaleProjected = 0;
     this._reflect = Math.random() * (Math.PI) - Math.PI * 0.5;
 
-    this._surface = 4/3 * Math.PI * Math.pow(this._radius, 3);
+    this._surface = 4/3 * Math.PI * Math.pow(convertPixelToMeter(this._radius), 3);
   }
 
   update = (w, h) => {
-    this._perspective = w * 0.9;
+    this._perspective = w * 1;
     this._projection_center_x = w / 2;
     this._projection_center_y = h / 2;
     this._x *= w / this._width;
@@ -223,7 +196,7 @@ class Bubble {
   }
 
   /**
-   * Update position using vectors
+   * Update position
    * @param {Float} x 
    * @param {Float} y 
    * @param {Float} z 
@@ -261,37 +234,30 @@ class Bubble {
   }
 }
 
-window.onload = () => {
-  const engine = new Engine(document.body);
-  engine.createBubble(100, 300);
-
-  window.requestAnimationFrame(engine.render);
+function convertPixelToMeter(p) {
+  return p / 3779.52755906;
 }
 
-// function collisionDetection() {
-//   for(var c=0; c<brickColumnCount; c++) {
-//     for(var r=0; r<brickRowCount; r++) {
-//       var b = bricks[c][r];
-//       if(b.status == 1) {
-//         if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-//           dy = -dy;
-//           b.status = 0;
-//           score++;
-//           if(score == brickRowCount*brickColumnCount) {
-//             alert("YOU WIN, CONGRATS!");
-//             document.location.reload();
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
+function convertMeterToPixel(m) {
+  return m * 3779.52755906;
+}
 
-// function draw() {
-//   // draw
-//   // collision detection
+function render(callback) {
+  callback();
+  window.requestAnimationFrame(() => render(callback))
+}
 
-//   requestAnimationFrame(draw);
-// }
+window.onload = () => {
+  const air_engine = new Engine(document.getElementById("air"), undefined, undefined, "air");
+  air_engine.createBubble(20, 300);
 
-// draw();
+  const water_engine = new Engine(document.getElementById("water"), undefined, undefined, "water");
+  water_engine.createBubble(20, 300);
+
+  window.requestAnimationFrame(() => {
+    render(() => {
+      air_engine.render();
+      water_engine.render();
+    });
+  });
+}
